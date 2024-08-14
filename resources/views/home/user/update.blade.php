7980009bd886a98609354488807bb9b1c24b91eb
GIT_Sebas <wowco.2000@gmail.com>
2024-08-14 13:02:34 -0500

    bug de editar corregido, antes solo me estaba editando a los aspirantes pero era porque el campo de la fecha era required y ya lo cambie a nullable para cuando se edite a un rol diferente de aspirante no haya problemas

--- resources/views/home/user/update.blade.php
+++ resources/views/home/user/update.blade.php
@@ -265,7 +265,7 @@
                     class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                     placeholder=" " />
                 </div>
-                @error('password')
+                @error('test_date')
                     <p class="mt-2 text-sm text-red-600 dark:text-red-500"><span class="font-medium">Incorrecto</span></p>
                 @enderror
             </div>
